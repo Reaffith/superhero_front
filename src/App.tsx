3c97762640649ba208d4d0be7d6382a1c79c5a40
Taras Nechyporuck <tarasnechyporuck@gmail.com>
2025-08-20 15:30:38 +0300

    added hero page

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import "./App.css";
 import { MainPage } from "./components/MainPage/MainPage";
 import { HeroesList } from "./components/HeroesList/HeroesList";
 import { CreateUpdatePage } from "./components/CreateUpdatePage/CreateUpdatePage";
+import { HeroPage } from "./components/HeroPage/HeroPage";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <Route index element={<HeroesList />}></Route>
           <Route path="create" element={<CreateUpdatePage />}></Route>
           <Route path="update/:id" element={<CreateUpdatePage />}></Route>
+          <Route path="hero/:id" element={<HeroPage/>}></Route>
         </Route>
       </Routes>
     </Router>
